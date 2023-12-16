285030029b528f4c759fa7a67fec3f714c2c8c13
afthztrk <153006213+afthztrk@users.noreply.github.com>
2023-12-16 13:38:28 +0300

    score and highscore
    score and highscore

--- TappyFish/Assets/Scripts/GameManager.cs
+++ TappyFish/Assets/Scripts/GameManager.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static Vector2 bottomLeft;
     public static bool gameOver;
+    public GameObject gameOverPanel;
     private void Awake()
     {
         bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+    }
+
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     // Start is called before the first frame update
@@ -20,6 +27,7 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOver = true;
+        gameOverPanel.SetActive(true);
     }
 
     // Update is called once per frame
